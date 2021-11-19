65a6c295c39320d5fb558c00ea3b53a9db4763a6
erzaffasya <58721335+erzaffasya@users.noreply.github.com>
2021-11-19 23:49:44 +0700

    revisi auth

--- admin/kamar/index.php
+++ admin/kamar/index.php
@@ -3,8 +3,9 @@ include "../../config.php";
 
 session_start();
  
-if (!isset($_SESSION['username'])) {
-    header("Location: index.php");
+if ($_SESSION['username'] == null) {
+   header("Location: ../../login.php");
+   exit();
 }
  
 ?>
