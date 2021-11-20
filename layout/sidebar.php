f1bbfd5de22ccacaeb4cd858069afb2795ea7b3e
erzaffasya <58721335+erzaffasya@users.noreply.github.com>
2021-11-20 13:01:53 +0700

    puush

--- layout/sidebar.php
+++ layout/sidebar.php
@@ -2,67 +2,62 @@
     <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
         <div class="sb-sidenav-menu">
             <div class="nav">
-                <div class="sb-sidenav-menu-heading">Core</div>
-                <a class="nav-link" href="index.html">
-                    <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
-                    Dashboard
-                </a>
-                <div class="sb-sidenav-menu-heading">Internal</div>
-                <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#kamar" aria-expanded="false" aria-controls="collapsePages">
-                    <div class="sb-nav-link-icon"><i class="fas fa-book-open"></i></div>
-                    Kamar
-                    <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
-                </a>
-                <div class="collapse" id="kamar" aria-labelledby="headingTwo" data-bs-parent="#sidenavAccordion">
-                <nav class="sb-sidenav-menu-nested nav">
-                        <a class="nav-link" href="../kamar/index.php">Data Kamar</a>
-                        <a class="nav-link" href="../kamar/tambah.php">Tambah Data Kamar</a>
-                    </nav>
-                </div>
-                <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#tempat-tidur" aria-expanded="false" aria-controls="collapsePages">
-                    <div class="sb-nav-link-icon"><i class="fas fa-book-open"></i></div>
-                    Tempat Tidur
-                    <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
-                </a>
-                <div class="collapse" id="tempat-tidur" aria-labelledby="headingTwo" data-bs-parent="#sidenavAccordion">
-                    <nav class="sb-sidenav-menu-nested nav">
-                        <a class="nav-link" href="../tempat-tidur/index.php">Data Tempat Tidur</a>
-                        <a class="nav-link" href="../tempat-tidur/tambah.php">Tambah Data Tempat Tidur</a>
-                    </nav>
-                </div>
-                <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#data-pengguna" aria-expanded="false" aria-controls="collapsePages">
-                    <div class="sb-nav-link-icon"><i class="fas fa-book-open"></i></div>
-                    Data Pengguna
-                    <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
-                </a>
-                <div class="collapse" id="data-pengguna" aria-labelledby="headingTwo" data-bs-parent="#sidenavAccordion">
-                    <nav class="sb-sidenav-menu-nested nav">
-                        <a class="nav-link" href="../pengguna/index.php">Data Pengguna</a>
-                        <a class="nav-link" href="../pengguna/tambah.php">Tambah Data Pengguna</a>
-                    </nav>
-                </div>
-                <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#jenis-kamar" aria-expanded="false" aria-controls="collapsePages">
-                    <div class="sb-nav-link-icon"><i class="fas fa-book-open"></i></div>
-                    Jenis Kamar
-                    <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
-                </a>
-                <div class="collapse" id="jenis-kamar" aria-labelledby="headingTwo" data-bs-parent="#sidenavAccordion">
-                    <nav class="sb-sidenav-menu-nested nav">
-                        <a class="nav-link" href="../jenis-kamar/index.php">Data Jenis Kamar</a>
-                        <a class="nav-link" href="../jenis-kamar/tambah.php">Tambah Data Jenis Kamar</a>
-                    </nav>
-                </div>
-                <!-- <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#tarif" aria-expanded="false" aria-controls="collapsePages">
-                    <div class="sb-nav-link-icon"><i class="fas fa-book-open"></i></div>
-                    Tarif
-                    <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
-                </a> -->
-                <!-- <div class="collapse" id="tarif" aria-labelledby="headingTwo" data-bs-parent="#sidenavAccordion">
-                    <nav class="sb-sidenav-menu-nested nav">
-                        <a class="nav-link" href="../tarif/index.php">Data Tarif</a>
-                        <a class="nav-link" href="../tarif/tambah.php">Tambah Data Tarif</a>
-                    </nav>
-                </div> -->
+                <?php
+                if ($_SESSION['role'] == 'admin') {
+                ?>
+                    <div class="sb-sidenav-menu-heading">Core</div>
+                    <a class="nav-link" href="index.html">
+                        <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
+                        Dashboard
+                    </a>
+                    <div class="sb-sidenav-menu-heading">Internal</div>
+                    <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#kamar" aria-expanded="false" aria-controls="collapsePages">
+                        <div class="sb-nav-link-icon"><i class="fas fa-book-open"></i></div>
+                        Kamar
+                        <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
+                    </a>
+                    <div class="collapse" id="kamar" aria-labelledby="headingTwo" data-bs-parent="#sidenavAccordion">
+                        <nav class="sb-sidenav-menu-nested nav">
+                            <a class="nav-link" href="../kamar/index.php">Data Kamar</a>
+                            <a class="nav-link" href="../kamar/tambah.php">Tambah Data Kamar</a>
+                        </nav>
+                    </div>
+                    <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#tempat-tidur" aria-expanded="false" aria-controls="collapsePages">
+                        <div class="sb-nav-link-icon"><i class="fas fa-book-open"></i></div>
+                        Tempat Tidur
+                        <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
+                    </a>
+                    <div class="collapse" id="tempat-tidur" aria-labelledby="headingTwo" data-bs-parent="#sidenavAccordion">
+                        <nav class="sb-sidenav-menu-nested nav">
+                            <a class="nav-link" href="../tempat-tidur/index.php">Data Tempat Tidur</a>
+                            <a class="nav-link" href="../tempat-tidur/tambah.php">Tambah Data Tempat Tidur</a>
+                        </nav>
+                    </div>
+                    <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#data-pengguna" aria-expanded="false" aria-controls="collapsePages">
+                        <div class="sb-nav-link-icon"><i class="fas fa-book-open"></i></div>
+                        Data Pengguna
+                        <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
+                    </a>
+                    <div class="collapse" id="data-pengguna" aria-labelledby="headingTwo" data-bs-parent="#sidenavAccordion">
+                        <nav class="sb-sidenav-menu-nested nav">
+                            <a class="nav-link" href="../pengguna/index.php">Data Pengguna</a>
+                            <a class="nav-link" href="../pengguna/tambah.php">Tambah Data Pengguna</a>
+                        </nav>
+                    </div>
+                    <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#jenis-kamar" aria-expanded="false" aria-controls="collapsePages">
+                        <div class="sb-nav-link-icon"><i class="fas fa-book-open"></i></div>
+                        Jenis Kamar
+                        <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
+                    </a>
+                    <div class="collapse" id="jenis-kamar" aria-labelledby="headingTwo" data-bs-parent="#sidenavAccordion">
+                        <nav class="sb-sidenav-menu-nested nav">
+                            <a class="nav-link" href="../jenis-kamar/index.php">Data Jenis Kamar</a>
+                            <a class="nav-link" href="../jenis-kamar/tambah.php">Tambah Data Jenis Kamar</a>
+                        </nav>
+                    </div>
+                <?php } ?>
+
+
                 <div class="sb-sidenav-menu-heading">Eksternal</div>
                 <a class="nav-link" href="../pelanggan/index.php">
                     <div class="sb-nav-link-icon"><i class="fas fa-chart-area"></i></div>
@@ -72,10 +67,7 @@
                     <div class="sb-nav-link-icon"><i class="fas fa-table"></i></div>
                     Tambah Data Tamu
                 </a>
-                <a class="nav-link" href="tables.html">
-                    <div class="sb-nav-link-icon"><i class="fas fa-table"></i></div>
-                    Data Reservasi
-                </a>
+
                 <a class="nav-link" href="../pembayaran/index.php">
                     <div class="sb-nav-link-icon"><i class="fas fa-table"></i></div>
                     Pembayaran
